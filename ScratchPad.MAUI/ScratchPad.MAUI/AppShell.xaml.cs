a5dccc24eacc2f7a071a47b8506714933935a224
Philip Heggelund <59456945+DuckScapePhilip@users.noreply.github.com>
2022-08-29 11:14:19 +0100

    Added in MAUI bug report.
    Added in MAUI bug report.

---
+++ ScratchPad.MAUI/ScratchPad.MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ScratchPad.MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ScratchPad.MAUI/ScratchPad.MAUI/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace ScratchPad.MAUI;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+
+        picker.ItemsSource = new List<TestItem>() { new TestItem("hello"), new TestItem("world") };
+    }
+}
+
+class TestItem
+{
+    public string Name { get; set; }
+
+    public TestItem(string name)
+    {
+        this.Name = name;
+    }
+
+    public override string ToString()
+    {
+        return this.Name.ToString();
+    }
+}
+
